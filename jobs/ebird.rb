d45a53df75f5b331b9bedc76ca421416b1d01242
schamberlain <myrmecocystus@gmail.com>
2013-03-02 22:26:34 -0800

    fixed ebird widget

--- jobs/ebird.rb
+++ jobs/ebird.rb
@@ -1,9 +1,9 @@
 require 'httparty'
 
 SCHEDULER.every '60s', :first_in => 0 do |job|
-  result = HTTParty.get("http://ebird.org/ws1.1/data/obs/geo/recent?lng=-76.51&lat=42.46&dist=2&back=5&maxResults=1&locale=en_US&fmt=json")
-
-	if result[0].key(4) == "howMany"
+  result = HTTParty.get('http://ebird.org/ws1.1/data/obs/geo_spp/recent?lng=-70.51&lat=42.4&sci=branta%20canadensis&fmt=json')
+  
+	if esult[0]['comName'] == "Canada Goose"
 			# out = "BOOM! It's up"
 			out = 100
 		else
