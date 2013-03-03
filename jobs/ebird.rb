b68374d6937e7df819c98358350cf91a61f443cc
schamberlain <myrmecocystus@gmail.com>
2013-03-02 22:32:16 -0800

    fixed bird call

--- jobs/ebird.rb
+++ jobs/ebird.rb
@@ -3,7 +3,7 @@ require 'httparty'
 SCHEDULER.every '60s', :first_in => 0 do |job|
   result = HTTParty.get('http://ebird.org/ws1.1/data/obs/geo_spp/recent?lng=-70.51&lat=42.4&sci=branta%20canadensis&fmt=json')
   
-	if esult[0]['comName'] == "Canada Goose"
+	if result[0]['comName'] == "Canada Goose"
 			# out = "BOOM! It's up"
 			out = 100
 		else
