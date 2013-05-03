8b6b2ac54ad7d0ec19bab4a72fcf7cc99c4c7e4a
schamberlain <myrmecocystus@gmail.com>
2013-05-03 14:30:52 -0700

    incorporated simons changes

--- R/get_download.R
+++ R/get_download.R
@@ -8,23 +8,19 @@
 #' @param datasetid Dataset ID, as returned by \code{get_datasets}.
 #' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
 #' @return This command returns either a 'try-error' definined by the error returned 
-#'    from the Neotoma API call, or a list comprising the following values:
+#'    from the Neotoma API call, or a list comprising the following items:
 #'    
 #' \itemize{
-#'  \item{DatasetID}{Unique database record identifier for the dataset.}
-#'  \item{DatasetName}{Name of the dataset; not commonly used.}
-#'  \item{CollUnitHandle}{Code name of the Collection Unit with which the dataset is associated. This code may be up to 10 characters. Data are frequently distributed by Collection Unit, and the Handle is used for file names.}
-#'  \item{CollUnitType}{The collection type. Types include cores, sections, excavations, and animal middens.}
-#'  \item{DatasetType}{The dataset type, such as: geochronologic, loss-on-ignition, pollen, plant macrofossils, vertebrate fauna, etc.}
-#'  \item{NeotomaLastSub}{The date of the most recent submission.}
-#'  \item{DefChronologyID}{The unique database identifier for the default chronology.}
-#'  \item{DatasetPIs}{An array of objects that describe Principal Investigators associated with a dataset}
-#'  \item{Site}{  An object describing the site where the dataset samples were taken}
-#'  \item{Samples}{  An array of objects describing the individual dataset samples}
+#'  \item{metadata}{A table describing the collection, including the dataset ID, dataset name, collection type, collection handle and dataset type.}
+#'  \item{pi.data}{A list of PI data, compatable with \code{get_contacts}.}
+#'  \item{site.data}{Site information, compatable with \code{get_sites}}
+#'  \item{sample.meta}{Dataset information for the core, primarily the age-depth model and chronology.}
+#'  \item{taxon.list}{The list of taxa contained within the dataset, unordered, including information that can be used in \code{get_taxa}}
+#'  \item{counts}{The assemblage data for the dataset, arranged with each successive depth in rows and the taxa as columns.  All taxa are described in \code{taxon.list}, the chronology is in \code{sample.data}}
 #' }
 #'    
-#'    Here the \code{Samples} is the real prize, but at present the format of 
-#'    the data object is simply a list of lists.  This needs work.
+#'    A full data object containing all the relevant assemblage information and metadata neccessary to understand a site.
+#'    The data object is a list of lists and data.frames that describe an assemblage, the constituent taxa, the chronology, site and PIs who contributed the data.
 #' @examples \dontrun{
 #' #  Search for sites with "Thuja" pollen that are older than 8kyr BP and
 #' #  that are on the west coast of North America:
@@ -32,6 +28,17 @@
 #' 
 #' #  Returns 3 records (as of 04/04/2013), get dataset for the first record, Gold Lake Bog.
 #' GOLDKBG <- get_download(t8kyr.datasets[[1]]$DatasetID)
+#' 
+#' taxa.no <- nrow(GOLDKBG$taxon.list)
+#'
+#' #  Of the 60 taxa in the record, plot the pollen curve for Abies over time:
+#' 
+#' pollen <- GOLDKBG$taxon.list$VariableElement == 'pollen'
+#' pol.curve <- data.frame(age = GOLDKBG$sample.meta$Age,
+#'                         Abies = GOLDKBG$counts[,'Abies'] / rowSums(GOLDKBG$counts[,pollen]))
+#' plot(Abies * 100 ~ age, data = pol.curve, type='b', 
+#'      ylab = '% Abies', xlab='Calibrated Years BP', pch=19)
+#'
 #' }
 #' @references
 #' Neotoma Project Website: http://www.neotomadb.org
@@ -52,8 +59,7 @@ get_download <- function(datasetid){
     if(!is.numeric(datasetid)) stop('datasetid must be numeric.')
   }
   
-  aa <- try(fromJSON(paste(base.uri, '/', datasetid, sep=''), nullValue = NA))
-  #getForm(base.uri, .params = cl)))
+  aa <- try(fromJSON(paste(base.uri, '/', cl, sep=''), nullValue = NA))
   
   if(aa[[1]] == 0){
     stop(paste('Server returned an error message:\n', aa[[2]]), call.=FALSE)
@@ -61,9 +67,49 @@ get_download <- function(datasetid){
   if(aa[[1]] == 1){
     aa <- aa[[2]]
     cat('The API call was successful, you have returned ', length(aa), 'records.\n')
+    
+    #  So here the goal is to reduce this list of lists to as simple a set of
+    #  matrices as possible.
+    if('Samples' %in% names(aa[[1]])){
+      meta.data <- with(aa[[1]], data.frame(dataset.id = DatasetID,
+                                            dataset.name = DatasetName,
+                                            collection.type = CollUnitType,
+                                            collection.handle = CollUnitHandle,
+                                            dataset.type = DatasetType))
+      site.data <- as.data.frame(aa[[1]]$Site)
+      site.data <- site.data[,c('SiteID', 'SiteName', 'Altitude',
+                                'LatitudeNorth', 'LongitudeWest', 
+                                'LatitudeSouth', 'LongitudeEast',
+                                'SiteDescription', 'SiteNotes')]
+      
+      pi.data <- aa[[1]]$DatasetPIs
+      
+      #  This is the part I'm less sure about doing nicely:
+      samples <- aa[[1]]$Samples
+      sample.meta <- data.frame(depths = sapply(samples, function(x) x$AnalysisUnitDepth),
+                                thickness = sapply(samples, function(x) x$AnalysisUnitName),
+                                ldply(samples, function(x) data.frame(x$SampleAges)),
+                                IDs = sapply(samples, function(x) x$SampleID),
+                                sample.name = sapply(samples, function(x) {
+                                  if(is.null(x$SampleUnitName)) NA
+                                  else x$SampleUnitName}),
+                                unit.name = sapply(samples, function(x) x$AnalysisUnitName))
+      
+      sample.data <- suppressMessages(melt(llply(samples, function(x) ldply(x$SampleData, data.frame))))
+      taxon.list <- sample.data[!duplicated(sample.data[,1]),1:5]
+      
+      count.table <- xtabs(value ~ L1 + TaxonName, sample.data)
+      
+      aa <- list(metadata = meta.data,
+                 pi.data = pi.data,
+                 site.data = site.data,
+                 sample.meta = sample.meta,
+                 taxon.list = taxon.list,
+                 counts = count.table)
+    }
   }
   
-  if(class(aa) == 'try-error') aa <- neotoma.form
+  if(class(aa) == 'try-error') aa <- aa
   
   aa
   
