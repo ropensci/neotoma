03b58685ca548c14bf6d490f8875b987d512aa72
schamberlain <myrmecocystus@gmail.com>
2013-07-12 07:05:48 -0700

    removed merge conflict lines from man and dot r file for get_download, resolves #15

--- R/get_download.R
+++ R/get_download.R
@@ -94,11 +94,7 @@ get_download <- function(datasetid){
                                   if(is.null(x$SampleUnitName)) NA
                                   else x$SampleUnitName}),
                                 unit.name = sapply(samples, function(x) x$AnalysisUnitName))
-<<<<<<< HEAD
 
-=======
-
->>>>>>> 9a7a9118a815defe72427557322d5a304b85e28d
       sample.data <- suppressMessages(melt(llply(samples, function(x) ldply(x$SampleData, data.frame))))
       taxon.list <- sample.data[!duplicated(sample.data[,1]),1:5]
 
@@ -117,8 +113,5 @@ get_download <- function(datasetid){
 
   aa
 
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 9a7a9118a815defe72427557322d5a304b85e28d
