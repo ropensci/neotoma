3dbe1af2289b2af793d3f25bb6b1fe6f1e61fa0b
Gavin Simpson <ucfagls@gmail.com>
2013-07-16 13:23:22 -0600

    big speed up in get_sites; remove plyr dependence

--- R/get_sites.R
+++ R/get_sites.R
@@ -1,19 +1,19 @@
 #' Return Site Information.
-#' 
-#' \code{get_sites} returns site information from the Neotoma Paleoecological Database 
+#'
+#' \code{get_sites} returns site information from the Neotoma Paleoecological Database
 #'    based on parameters defined by the user.
-#' 
-#' @import RJSONIO RCurl plyr
+#'
+#' @import RJSONIO RCurl
 #' @param siteid The numerical site ID.
 #' @param sitename A character string representing the full or partial site name.
 #' @param altmin Minimum site altitude  (in m).
 #' @param altmax Maximum site altitude (in m).
 #' @param loc A numeric vector c(lonW, latS, lonE, latN) representing the bounding box within which to search for sites.  The convention here is to use negative values for longitudes west of Grewnwich or longitudes south of the equator.
 #' @param gpid A character string, must correspond to a valid geopolitical identity in the Neotoma Database.  Use get.tables('GeoPoliticalUnits') for a list of acceptable values, or link here: http://api.neotomadb.org/apdx/geopol.htm
-#' 
+#'
 #' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
 #' @return A table:
-#'    
+#'
 #' \itemize{
 #'  \item{SiteID}{Unique database record identifier for the site.}
 #'  \item{SiteName}{Name of the site.}
@@ -24,7 +24,7 @@
 #'  \item{LongitudeWest}{West bounding longitude, in decimal degrees, for a site.}
 #'  \item{SiteDescription}{Free form description of a site, including such information as physiography and vegetation around the site.}
 #' }
-#'    
+#'
 #' Extended response variables when only a single site is returned:
 #' \itemize{
 #'  \item{CollectionUnitID}{Unique database record identifier for the collection unit.}
@@ -35,7 +35,7 @@
 #' @examples \dontrun{
 #' #  What is the distribution of site elevations in Neotoma?
 #' all.sites <- get_sites()  #takes a bit of time.
-#' 
+#'
 #' plot(density(all.sites$Altitude, from = 0, na.rm=TRUE),
 #' main = 'Altitudinal Distribution of Neotoma Sites', xlab = 'Altitude (m)', log='x')
 #' }
@@ -43,20 +43,20 @@
 #' Neotoma Project Website: http://www.neotomadb.org
 #' API Reference:  http://api.neotomadb.org/doc/resources/contacts
 #' @keywords Neotoma Palaeoecology API
-#' @export 
+#' @export
 get_sites <- function(siteid, sitename, altmin, altmax, loc, gpid){
-  
+
   base.uri <- 'http://api.neotomadb.org/v1/data/sites'
-  
+
   cl <- as.list(match.call())
   cl[[1]] <- NULL
   cl <- lapply(cl, eval, envir=parent.frame())
-  
+
   #  Parameter check on siteid:
   if('siteid' %in% names(cl)){
     if(!is.numeric(siteid)) stop('siteid must be numeric.')
   }
-  
+
   #  Parameter check on altitudes.  This gets reused, we could turn it into a
   #  higher level function to save reading lame code:
   if(all(c('altmin', 'altmax') %in% names(cl))){
@@ -68,16 +68,16 @@ get_sites <- function(siteid, sitename, altmin, altmax, loc, gpid){
       warning('altmin must be smaller than atmax, values were switched in call.')
     }
   }
-  
-  # Parameter check on 'loc', ought to be a comma separated list of 
+
+  # Parameter check on 'loc', ought to be a comma separated list of
   # lonW, latS, lonE, latN when it is passed out, but it's probably
   # better to pass in a vector.  Although it might be better to associate
   # it with a spatial object existing in R like an extent or bbox.
   if('loc' %in% names(cl)){
     cl$loc <- eval(cl$loc)
-    
+
     if(class(cl$loc) == 'numeric' & length(cl$loc == 4)){
-      
+
       #  The latitudes must be from -90 to 90
       #  The longitudes must be from -180 to 180
       if(all(findInterval(cl$loc[c(2,4)], c(-90, 90)) == 1) &
@@ -92,7 +92,7 @@ get_sites <- function(siteid, sitename, altmin, altmax, loc, gpid){
       stop('The loc must be a numeric vector: lonW, latS, lonE, latN.\n')
     }
   }
-  
+
   #  Parameter check on 'gpid', the name needs to be in the big table in
   #  data object geopol:
   if('gpid' %in% names(cl)){
@@ -100,10 +100,10 @@ get_sites <- function(siteid, sitename, altmin, altmax, loc, gpid){
     #  stop('Unrecognized geopolitical entity.  Check for acceptible names in data(geopol).')
     #}
   }
-  
+
   neotoma.form <- getForm(base.uri, .params = cl)
   aa <- try(fromJSON(neotoma.form, nullValue = NA))
-  
+
   if(aa[[1]] == 0){
     stop(paste('Server returned an error message:\n', aa[[2]]), call.=FALSE)
   }
@@ -111,15 +111,19 @@ get_sites <- function(siteid, sitename, altmin, altmax, loc, gpid){
     aa <- aa[[2]]
     cat('The API call was successful, you have returned ', length(aa), 'records.\n')
   }
-  
+
   if(class(aa) == 'try-error') output <- neotoma.form
   else{
     if('siteid' %in% names(cl) & length(names(cl)) == 1){
-      
+
     }
     else{
-      names(aa) <- sapply(aa, function(x)x$SiteName)
-      output <- suppressMessages( do.call('rbind', lapply(aa, data.frame, stringsAsFactors = FALSE)))
+      names(aa) <- sapply(aa, `[[`, "SiteName")
+      ## This is much faster by direct calling of the data frame method
+      ## of rbind
+      output2 <- do.call(rbind.data.frame, aa)
+      output2$SiteName <- as.character(output2$SiteName)
+      output2$SiteDescription <- as.character(output2$SiteDescription)
     }
   }
   output
