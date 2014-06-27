bd11a68a585910eb93635e73b2b212a7eccd9bb2
Karthik Ram <karthik.ram@gmail.com>
2014-06-26 20:45:30 -0700

    Added documentation for neotoma package. Solves #83
    Also bumped up version number

---
+++ R/neotoma-package.R
@@ -0,0 +1,5 @@
+#' @title neotoma
+#' @name neotoma
+#' @docType package
+#' @details The neotoma package is a programmatic R interface to the \href{http://www.neotomadb.org/}{Neotoma Paleoecological Database}. The package is intended to both allow users to search for sites and to download data for use in analyical workflows of paleoecological research.
+NULL
