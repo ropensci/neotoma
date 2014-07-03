bf1606f608c07ecf72f01aba1ffa19cdeaab95d8
unknown <willlab@GEO-201-001.shc.wisc.edu>
2014-07-03 10:27:31 -0500

    Brought get_contact up to R style guide standard.

--- R/get_contact.R
+++ R/get_contact.R
@@ -58,38 +58,37 @@ get_contact <- function(contactid, contactname, contactstatus, familyname){
 
   cl <- as.list(match.call())
   cl[[1]] <- NULL
-  cl <- lapply(cl, eval, envir=parent.frame())
+  cl <- lapply(cl, eval, envir = parent.frame())
 
-  #  Parameter check on contactid:
-  if('contactid' %in% names(cl)){
-    if(!is.numeric(cl$contactid)){
+  # Parameter check on contactid:
+  if ('contactid' %in% names(cl)){
+    if (!is.numeric(cl$contactid)){
       stop('The contactid must be numeric.')
     }
   }
 
-  #  Parameter check on contactname:
-  if('contactname' %in% names(cl)){
-    if(!is.character(cl$contactname)){
+  # Parameter check on contactname:
+  if ('contactname' %in% names(cl)){
+    if (!is.character(cl$contactname)){
       stop('The contactname must be a character string.')
     }
   }
 
-  #  Parameter check on contactstatus:
-  if('contactstatus' %in% names(cl)){
-    if(!is.character(cl$contactstatus)){
+  # Parameter check on contactstatus:
+  if ('contactstatus' %in% names(cl)){
+    if (!is.character(cl$contactstatus)){
       stop('The contactstatus must be a character string.')
-    }
-    else{
-      if(!cl$contactstatus %in% c('active', 'deceased', 'defunct',
+    } else {
+      if (!cl$contactstatus %in% c('active', 'deceased', 'defunct',
                                   'extant', 'inactive', 'retired', 'unknown')){
         stop('status must be an accepted term.  Use get.table(\'ContactStatues\')')
       }
     }
   }
 
-  #  Parameter check on familyname:
-  if('familyname' %in% names(cl)){
-    if(!is.character(cl$familyname)){
+  # Parameter check on familyname:
+  if ('familyname' %in% names(cl)){
+    if (!is.character(cl$familyname)){
       stop('The familyname must be a character string.')
     }
   }
@@ -97,16 +96,18 @@ get_contact <- function(contactid, contactname, contactstatus, familyname){
   neotoma.form <- getForm(base.uri, .params = cl)
   aa <- try(fromJSON(neotoma.form, nullValue = NA))
 
-  if(aa[[1]] == 0){
-    stop(paste('Server returned an error message:\n', aa[[2]]), call.=FALSE)
+  if (aa[[1]] == 0){
+    stop(paste('Server returned an error message:\n', aa[[2]]), call. = FALSE)
   }
-  if(aa[[1]] == 1){
+  if (aa[[1]] == 1){
     aa <- aa[[2]]
-    message(paste0('The API call was successful, you have returned ', length(aa), ' records.\n'))
+    message(paste0('The API call was successful, you have returned ', 
+                   length(aa), ' records.\n'))
   }
 
-  if(class(aa) == 'try-error') output <- neotoma.form
-  else{
+  if (class(aa) == 'try-error') {
+    output <- neotoma.form
+  } else {
     names(aa) <- sapply(aa, function(x)x$ContactID)
     output <- melt(lapply(aa, data.frame),
                    id.vars = c("Address", "URL", "GivenNames",
@@ -116,7 +117,7 @@ get_contact <- function(contactid, contactname, contactstatus, familyname){
     output <- dcast(output,
                     formula = ContactName + ContactStatus + FamilyName +
                     LeadingInitials + GivenNames + Suffix + Title + Phone +
-                    Fax + Email + URL + Address + Notes ~ variable) ##[,-2]
+                    Fax + Email + URL + Address + Notes ~ variable) ##[, -2]
   }
 
   output
