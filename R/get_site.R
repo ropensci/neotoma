8f013065f03f93ca2f30f0eee48d4e28d8858891
unknown <willlab@GEO-201-001.shc.wisc.edu>
2014-07-16 16:45:18 -0500

    Brought get_download, get_geochron, get_publication, get_site, and get_table up to Google's R Style Guide Standards.

--- R/get_site.R
+++ R/get_site.R
@@ -42,19 +42,19 @@ get_site <- function(sitename, altmin, altmax, loc, gpid){
 
   cl <- as.list(match.call())
   cl[[1]] <- NULL
-  cl <- lapply(cl, eval, envir=parent.frame())
+  cl <- lapply(cl, eval, envir = parent.frame())
 
-  #  Parameter check on siteid:
-  if('siteid' %in% names(cl)){
-    if(!is.numeric(siteid)) stop('siteid must be numeric.')
+  # Parameter check on siteid:
+  if ('siteid' %in% names(cl)){
+    if (!is.numeric(siteid)) stop('siteid must be numeric.')
   }
 
-  #  Parameter check on altitudes.  This gets reused, we could turn it into a
-  #  higher level function to save reading lame code:
-  if(all(c('altmin', 'altmax') %in% names(cl))){
-    #  If the user defines a minimum and maximum altitude, make sure that the
-    #  minimum is smaller than the max.
-    if(cl$altmin > cl$altmax){
+  # Parameter check on altitudes.  This gets reused, we could turn it into a
+  # higher level function to save reading lame code:
+  if (all(c('altmin', 'altmax') %in% names(cl))){
+    # If the user defines a minimum and maximum altitude, make sure that the
+    # minimum is smaller than the max.
+    if (cl$altmin > cl$altmax){
       altmin <- min(c(cl$altmin, cl$altmax))
       altmax <- max(c(cl$altmin, cl$altmax))
       warning('altmin must be smaller than atmax, values were switched in call.')
@@ -65,37 +65,35 @@ get_site <- function(sitename, altmin, altmax, loc, gpid){
   # lonW, latS, lonE, latN when it is passed out, but it's probably
   # better to pass in a vector.  Although it might be better to associate
   # it with a spatial object existing in R like an extent or bbox.
-  if('loc' %in% names(cl)){
+  if ('loc' %in% names(cl)){
     cl$loc <- eval(cl$loc)
 
-    if(class(cl$loc) == 'numeric' & length(cl$loc == 4)){
+    if (class(cl$loc) == 'numeric' & length(cl$loc == 4)){
 
-      #  The latitudes must be from -90 to 90
-      #  The longitudes must be from -180 to 180
-      if(all(findInterval(cl$loc[c(2,4)], c(-90, 90)) == 1) &
+      # The latitudes must be from -90 to 90
+      # The longitudes must be from -180 to 180
+      if (all(findInterval(cl$loc[c(2,4)], c(-90, 90)) == 1) &
            all(findInterval(cl$loc[c(1,3)], c(-180, 180)) == 1)){
         cl$loc <- paste(cl$loc, collapse = ',')
+      } else {
+        stop(paste0('loc must be in the form c(lonW, latS, lonE, latN).',
+                    '\nLongitudes from -180 to 180, latitudes from -90 to 90.'))
       }
-      else{
-        stop('loc must be in the form c(lonW, latS, lonE, latN).\nLongitudes from -180 to 180, latitudes from -90 to 90.')
-      }
-    }
-    else{
+    } else {
       stop('The loc must be a numeric vector: lonW, latS, lonE, latN.\n')
     }
   }
 
-  if('gpid' %in% names(cl)){
-    if(is.character(gpid)){
+  if ('gpid' %in% names(cl)){
+    if (is.character(gpid)){
       data(gp.table)
       gprow <- match(x=gpid, table=gp.table$GeoPoliticalName)
-      if(is.na(gprow)){
+      if (is.na(gprow)){
         stop('Cannot find a match for the gpid provided.')
       }
       gpid <- gp.table$GeoPoliticalID[gprow]
-    }
-    else{
-      if(!is.numeric(gpid)){
+    } else {
+      if (!is.numeric(gpid)){
         stop('The gpid must be either a character string or an integer.')
       }
     }
@@ -104,28 +102,30 @@ get_site <- function(sitename, altmin, altmax, loc, gpid){
   neotoma.form <- getForm(base.uri, .params = cl)
   aa <- try(fromJSON(neotoma.form, nullValue = NA))
 
-  if(aa[[1]] == 0){
-    stop(paste('Server returned an error message:\n', aa[[2]]), call.=FALSE)
+  if (aa[[1]] == 0){
+    stop(paste('Server returned an error message:\n', aa[[2]]), call. = FALSE)
   }
-  if(aa[[1]] == 1){
+  if (aa[[1]] == 1){
     aa <- aa[[2]]
-    cat('The API call was successful, you have returned ', length(aa), 'records.\n')
+    cat('The API call was successful, you have returned ',
+        length(aa), 'records.\n')
   }
 
-  if(class(aa) == 'try-error') output <- neotoma.form
-  else{
+  if (class(aa) == 'try-error'){
+     output <- neotoma.form
+  } else {
     names(aa) <- sapply(aa, `[[`, "SiteName")
-    ## This is much faster by direct calling of the data frame method
-    ## of rbind
+    # This is much faster by direct calling of the data frame method
+    # of rbind
     output <- do.call(rbind.data.frame, aa)
-    ## but we need to fix-up some characters that R changed to factors
+    # but we need to fix-up some characters that R changed to factors
     output$SiteName <- as.character(output$SiteName)
     output$SiteDescription <- as.character(output$SiteDescription)
     
     output <- data.frame(siteid = output$SiteID,
                 sitename = output$SiteName,
-                long = rowMeans(output[,c('LongitudeWest', 'LongitudeEast')], na.rm=TRUE),
-                lat = rowMeans(output[,c('LatitudeNorth', 'LatitudeSouth')], na.rm=TRUE),
+                long = rowMeans(output[, c('LongitudeWest', 'LongitudeEast')], na.rm = TRUE),
+                lat = rowMeans(output[, c('LatitudeNorth', 'LatitudeSouth')], na.rm = TRUE),
                 elev = output$Altitude,
                 description = output$SiteDescription,
                 long_acc = abs(output$LongitudeWest - output$LongitudeEast),
--- R/get_table.R
+++ R/get_table.R
@@ -24,53 +24,53 @@ get_table <- function(table.name = NULL){
 
   base.uri <- 'http://api.neotomadb.org/v1/dbtables/'
 
-  name.list <- c('AgeTypes','AggregateChronologies',
-                 'AggregateDatasets','AggregateOrderTypes',
-                 'AggregateSampleAges','AggregateSamples',
-                 'AnalysisUnits','ChronControls',
-                 'ChronControlTypes','Chronologies',
-                 'CollectionTypes','CollectionUnits',
-                 'Collectors','Contacts','ContactStatuses',
-                 'Data','DatasetPIs','DatasetPublications',
-                 'Datasets','DatasetSubmissions',
-                 'DatasetSubmissionTypes','DatasetTypes',
-                 'DepAgents','DepAgentTypes','DepEnvtTypes',
-                 'EcolGroups','EcolGroupTypes','EcolSetTypes',
-                 'FaciesTypes','Geochronology','GeochronPublications',
-                 'GeochronTypes','GeoPoliticalUnits','Keywords',
-                 'Lithology','Projects','PublicationAuthors',
-                 'PublicationEditors','Publications',
-                 'PublicationTypes','RelativeAgePublications',
-                 'RelativeAges','RadiocarbonCalibration',
-                 'RelativeAgeScales','RelativeAgeUnits',
-                 'RelativeChronology','RepositoryInstitutions',
-                 'RepositorySpecimens','SampleAges','SampleAnalysts',
-                 'SampleKeywords','Samples','SiteImages','Sites',
-                 'SiteGeoPolitical','SpecimenDates','Synonyms',
-                 'SynonymTypes','Taxa','TaxaGroupTypes',
-                 'Tephrachronology','Tephras','Variables',
-                 'VariableContexts','VariableElements',
-                 'VariableModifications','VariableUnits')
+  name.list <- c('AgeTypes', 'AggregateChronologies',
+                 'AggregateDatasets', 'AggregateOrderTypes',
+                 'AggregateSampleAges', 'AggregateSamples',
+                 'AnalysisUnits', 'ChronControls',
+                 'ChronControlTypes', 'Chronologies',
+                 'CollectionTypes', 'CollectionUnits',
+                 'Collectors', 'Contacts', 'ContactStatuses',
+                 'Data', 'DatasetPIs', 'DatasetPublications',
+                 'Datasets', 'DatasetSubmissions',
+                 'DatasetSubmissionTypes', 'DatasetTypes',
+                 'DepAgents', 'DepAgentTypes', 'DepEnvtTypes',
+                 'EcolGroups', 'EcolGroupTypes', 'EcolSetTypes',
+                 'FaciesTypes', 'Geochronology', 'GeochronPublications',
+                 'GeochronTypes', 'GeoPoliticalUnits', 'Keywords',
+                 'Lithology', 'Projects', 'PublicationAuthors',
+                 'PublicationEditors', 'Publications',
+                 'PublicationTypes', 'RelativeAgePublications',
+                 'RelativeAges', 'RadiocarbonCalibration',
+                 'RelativeAgeScales', 'RelativeAgeUnits',
+                 'RelativeChronology', 'RepositoryInstitutions',
+                 'RepositorySpecimens', 'SampleAges', 'SampleAnalysts',
+                 'SampleKeywords', 'Samples', 'SiteImages', 'Sites',
+                 'SiteGeoPolitical', 'SpecimenDates', 'Synonyms',
+                 'SynonymTypes', 'Taxa', 'TaxaGroupTypes',
+                 'Tephrachronology', 'Tephras', 'Variables',
+                 'VariableContexts', 'VariableElements',
+                 'VariableModifications', 'VariableUnits')
 
-  if(!is.null(table.name)){
+  if (!is.null(table.name)){
     is.match <- pmatch(toupper(table.name), toupper(name.list))
-    if(!is.na(is.match)){
+    if (!is.na(is.match)){
       table.name <- name.list[is.match]
 
-      ## Can use getURLContent here which avoids the issue of having
-      ## an empty param list. Really this fun is not submitting a form
-      ## so no need for getForm()
+      # Can use getURLContent here which avoids the issue of having
+      # an empty param list. Really this fun is not submitting a form
+      # so no need for getForm()
       aa <- try(fromJSON(getURLContent(paste0(base.uri, table.name,
                                               "?limit=all"),
                                        binary = FALSE),
                          nullValue = NA),
                 silent = TRUE)
 
-      if(aa[[1]] == 1){
+      if (aa[[1]] == 1){
         allnames <- unique(unlist(lapply(aa[[2]], names), use.names = FALSE))
 
-        ## this is slightly quicker, but advantage is that it maintains
-        ## logical variable Extinct in correct mode
+        # this is slightly quicker, but advantage is that it maintains
+        # logical variable Extinct in correct mode
         
         old.string <- getOption("stringsAsFactors") #I don't know how else to do this with the do.call command.
         options(stringsAsFactors = FALSE)
@@ -79,30 +79,31 @@ get_table <- function(table.name = NULL){
         
         options(stringsAsFactors = old.string)
 
-        ## get rid of the rownames
+        # Get rid of the rownames
         rownames(table) <- NULL
 
-      }
-      else{
-        warning(paste(aa[[2]], ' Returning list of acceptable variable names.', sep = ''))
+      } else {
+        warning(paste0(aa[[2]],
+                       ' Returning list of acceptable variable names.')
         aa <- list()
         aa[[1]] <- 0
       }
 
-    }
-    else{
-      warning(paste('Unable to find match for variable name: ', table.name, '. Returning list of acceptable variable names.', sep = ''))
+    } else {
+      warning(paste0('Unable to find match for variable name: ',
+                    table.name,
+                    '. Returning list of acceptable variable names.')
       aa <- list()
       aa[[1]] <- 0
     }
-  }
-  else{
-    warning(paste('No variable name supplied. Returning list of acceptable variable names.', sep = ''))
+  } else {
+    warning(paste0('No variable name supplied. Returning list of ',
+                   'acceptable variable names.')
     aa <- list()
     aa[[1]] <- 0
   }
 
-  if(aa[[1]] == 1){
+  if (aa[[1]] == 1){
       out <- table
   } else {
       out <- name.list
