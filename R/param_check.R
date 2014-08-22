fd15e0a2526edfc88509cbe7a472817b9e3de198
Gavin Simpson <ucfagls@gmail.com>
2014-08-22 09:35:39 -0600

    remove trailing white space

--- R/param_check.R
+++ R/param_check.R
@@ -17,10 +17,10 @@
 param_check <- function(cl){
   #  A long list of proper parameter formatting so that it's not stuck in all
   #  the basic functions.
-  
+
   error <- list(flag = 0,
                 message = list())
-  
+
   if ('ageof' %in% names(cl)){
     if (!cl$ageof %in% c('sample', 'taxon', 'dataset')){
       error$flag <- 1
@@ -48,7 +48,7 @@ param_check <- function(cl){
                                                            'values were switched in call.')
     }
   }
-  
+
   # Parameter check on altitudes.  This gets reused, we could turn it into a
   # higher level function to save reading lame code:
   if (all(c('altmin', 'altmax') %in% names(cl))){
@@ -59,7 +59,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'altmin must be smaller than altmax.'
     }
   }
-  
+
   # Parameter check on contactid:
   if ('contactid' %in% names(cl)){
     if (!is.numeric(cl$contactid)){
@@ -67,7 +67,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'The contactid must be numeric.'
     }
   }
-  
+
   # Parameter check on contactname:
   if ('contactname' %in% names(cl)){
     if (!is.character(cl$contactname)){
@@ -75,7 +75,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'The contactname must be a character string.'
     }
   }
-  
+
   #  Parameter check on contactstatus:
   if ('contactstatus' %in% names(cl)){
     if (!is.character(cl$contactstatus)){
@@ -90,7 +90,7 @@ param_check <- function(cl){
       }
     }
   }
-  
+
   # Parameter check on familyname:
   if ('familyname' %in% names(cl)){
     if (!is.character(cl$familyname)){
@@ -98,7 +98,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'The familyname must be a character string.'
     }
   }
-  
+
   #  Test the geographic identification against the Geopolitical name table.
   if ('gpid' %in% names(cl)){
     if (is.character(gpid)){
@@ -116,16 +116,16 @@ param_check <- function(cl){
       }
     }
   }
-  
+
   # Parameter check on 'loc', ought to be a comma separated list of
   # lonW, latS, lonE, latN when it is passed out, but it's probably
   # better to pass in a vector.  Although it might be better to associate
   # it with a spatial object existing in R like an extent or bbox.
   if ('loc' %in% names(cl)){
     cl$loc <- eval(cl$loc)
-    
+
     if (class(cl$loc) == 'numeric' & length(cl$loc == 4)){
-      
+
       # The latitudes must be from -90 to 90
       # The longitudes must be from -180 to 180
       if (all(findInterval(cl$loc[c(2,4)], c(-90, 90)) == 1) &
@@ -141,7 +141,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'The loc must be a numeric vector: lonW, latS, lonE, latN.\n'
     }
   }
-  
+
   #  Test the PI identification.
   if ('piid' %in% names(cl)){
     # piid must be the numeric PI id number in the Neotoma database.
@@ -150,7 +150,7 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] = 'piid must be a numeric value.'
     }
   }
-  
+
   # Parameter check on siteid:
   if ('siteid' %in% names(cl)){
     if (!is.numeric(cl$siteid)){
@@ -158,15 +158,15 @@ param_check <- function(cl){
       error$message[[length(error$message) + 1]] <- 'siteid must be numeric.'
     }
   }
-  
+
   if ('taxonname' %in% names(cl)){
     if (!class(cl$taxonname) == 'character'){
       error$flag <- 1
       error$message[[length(error$message) + 1]] <- 'The taxonname must be a character.'
     }
   }
-  
-    
+
+
   error
-  
+
 }
