9a100323a05da61c12455bd20559e5ee8ed8b10e
Gavin Simpson <ucfagls@gmail.com>
2014-08-22 10:39:12 -0600

    moved infor to the @details; links to functions

--- R/compile_downloads.R
+++ R/compile_downloads.R
@@ -1,17 +1,17 @@
 
 #' Function to convert multiple downloads into a single large table.
 #'
-#' From the assemblage data for multiple cores, return a single \code{data.frame} with columns for site
+#' From the assemblage data for multiple cores, return a single data.frame with columns for site
 #' metadata and assemblage data.
 #'
-#' @param downloads A list of downloads as returned by \code{get_download}, or mutliple sites joined in a list.
-#' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
-#' @return This command returns a \code{data.frame}
+#' To support further synthesis and analysis \code{compile_download} works to transform a list
+#' returned by \code{\link{get_download}} into a large data frame with columns for site and sample attributes
+#' and also with the associated assemblage data at each sample depth.  This function also does the same for
+#' single sites.
 #'
-#'  To support further synthesis and analysis \code{compile_download} works to transform a list
-#'  returned by \code{get_download} into a large \code{data.frame} with columns for site and sample attributes
-#'  and also with the associated assemblage data at each sample depth.  This function also does the same for
-#'  single sites.
+#' @param downloads A list of downloads as returned by \code{\link{get_download}}, or mutliple sites joined in a list.
+#' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
+#' @return This command returns a data frame.
 #'
 #' @examples \dontrun{
 #' #  Search for sites with "Thuja" pollen that are older than 8kyr BP and
@@ -24,30 +24,30 @@
 #' gold.p25 <- lapply(thuja.sites, compile_taxa, 'P25')
 #'
 #' all.gold <- compile_downloads(gold.p25)
-#' 
+#'
 #' }
 #' @references
 #' Neotoma Project Website: http://www.neotomadb.org
-#' 
+#'
 #' Gavin DG, Oswald WW, Wahl ER, Williams JW. 2003. A statistical approach to evaluating distance metrics and analog assignments for pollen records. Quaternary Research 60: 356-367.
 #'
 #' Whitmore J, Gajewski K, Sawada M, Williams JW, Shuman B, Bartlein PJ, Minckley T, Viau AE, Webb III T, Shafer S, Anderson P, Brubaker L. 2005. Modern pollen data from North America and Greenland for multi-scale paleoenvironmental applications. Quaternary Science Reviews 24: 1828-1848.
 #'
 #' Williams J, Shuman B. 2008. Obtaining accurate and precise environmental reconstructions from the modern analog technique and North American surface pollen dataset. Quaternary Science Reviews. 27:669-687.
-#' 
+#'
 #' API Reference:  http://api.neotomadb.org/doc/resources/contacts
 #' @keywords Neotoma Palaeoecology API
 #' @export
 
 compile_downloads <-function(downloads){
-  
+
   #  We're going to ldply the list to make a data.frame from key metadata, but
   #  first we need a function to turn a single download into a data.frame:
-  
+
   if(!'download' %in% class(downloads)){
     stop('compile_datasets can only operate on lists as returned from get_download')
   }
-  
+
   down.to.df <- function(x){
     if('download' %in% class(x)){
       #  There can be NULL values in the download object.  We'll turn them to NA values:
@@ -58,7 +58,7 @@ compile_downloads <-function(downloads){
       if(is.null(x$sample.meta$AgeYounger)) x$sample.meta$AgeYounger <- NA
       if(is.null(x$metadata$site.data$lat)) x$metadata$site.data$lat <- NA
       if(is.null(x$metadata$site.data$long)) x$metadata$site.data$long <- NA
-      
+
       site.info <- data.frame(sitename = x$metadata$site.data$sitename,
                               depth = x$sample.meta$depths,
                               age = x$sample.meta$Age,
@@ -85,14 +85,14 @@ compile_downloads <-function(downloads){
     }
     site.info
   }
-  
-  
+
+
   if('download' %in% class(downloads) & !'metadata'%in%names(downloads)){
     site.info <- ldply(downloads, down.to.df)
-  } 
+  }
   if('download' %in% class(downloads) & 'metadata'%in%names(downloads)){
     site.info <- down.to.df(downloads)
   }
-  
+
   return(site.info)
 }
