8f6de82adbe2a01065a6108694b005757a81f2b5
Simon Goring <simon.j.goring@gmail.com>
2014-09-17 15:23:44 -0500

    Roxygen code for the download class.

---
+++ R/download.R
@@ -0,0 +1,12 @@
+##' @title A class for download objects.
+##'
+##' @description \pkg{coenocliner} provides a simple, easy interface for simulating species abundance (counts) or occurrence along gradients.
+##'
+##' @details One of the key ways quantitative ecologists attempt to understand the properties and behaviour of the methods they use or dream up is through the use of simulated data. \pkg{coenocliner} is an R package that provides a simple interface to coenocline simulation.
+##'
+##' @author Gavin L. Simpson
+##' @docType package
+##' @name coenocliner
+##' @aliases coenocliner-package
+##' @keywords package
+##' @seealso \code{\link{coenocline}} for simulating species data, \code{\link{distributions}} for details of the error distributions tha can be used for simulations, and \code{\link{species-response}} for details on the available species response models and the parameters required to use them.
