2ffde3270f4e51117e82ff52cb3137903f7f7a63
Gavin Simpson <ucfagls@gmail.com>
2014-09-25 20:55:56 -0600

    add a method for download objects

--- R/counts.R
+++ R/counts.R
@@ -1,14 +1,19 @@
 ##' Extract pollen or other proxy counts from data objects and returns them in a useful format.
 ##'
-##' 
 ##'
-##' @title 
+##'
+##' @title
 ##' @param obj an R object from which counts are to be extracted.
-##' @param ... 
-##' @return 
+##' @param ...
+##' @return
 ##' @author Gavin Simpson
 `counts` <- function(obj, ...) {
     UseMethod("counts")
 }
 
-`
+`counts.download` <- function(obj, ...) {
+    ret <- obj[["counts"]]
+    class(ret) <- c("neo_counts", "data.frame")
+    ret
+}
+
