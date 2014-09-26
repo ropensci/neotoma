93c1392f63730ee6d96e6b8181956f1255836b70
Gavin Simpson <ucfagls@gmail.com>
2014-09-25 21:04:16 -0600

    add a method for download_list objects; pad out some of the roxygen tags

--- R/counts.R
+++ R/counts.R
@@ -1,19 +1,33 @@
 ##' Extract pollen or other proxy counts from data objects and returns them in a useful format.
 ##'
+##' Methods are available for "download" and "download_list" objects.
 ##'
+##' @title Access proxy count data
 ##'
-##' @title
 ##' @param obj an R object from which counts are to be extracted.
-##' @param ...
-##' @return
+##' @param ... arugments passed to other methods.
+##' @return Either a data frame of counts or a list of such objects.
+##'
 ##' @author Gavin Simpson
+##'
+##' @export
+##' @rdname counts
 `counts` <- function(obj, ...) {
     UseMethod("counts")
 }
 
+##' @export
+##' @rdname counts
 `counts.download` <- function(obj, ...) {
     ret <- obj[["counts"]]
     class(ret) <- c("neo_counts", "data.frame")
     ret
 }
 
+##' @export
+##' @rdname counts
+`counts.download_list` <- function(obj, ...) {
+    ret <- lapply(obj, counts)
+    class(ret) <- c("neo_counts_list", "list")
+    ret
+}
