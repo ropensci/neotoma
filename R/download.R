f7cba3a7d20eff6a4466d3df2a49f05bd4928323
Gavin Simpson <ucfagls@gmail.com>
2014-09-25 20:45:19 -0600

    Fix this file which was causing R CMD check to error out. This is still a stub.

--- R/download.R
+++ R/download.R
@@ -2,11 +2,11 @@
 ##'
 ##' @description A \code{download} is an object with the full record for a single dataset.
 ##'
-##' @details One of the key ways quantitative ecologists attempt to understand the properties and behaviour of the methods they use or dream up is through the use of simulated data. \pkg{coenocliner} is an R package that provides a simple interface to coenocline simulation.
+##' @details TO DO
 ##'
 ##' @author Simon Goring
 ##' @docType package
-##' @name coenocliner
-##' @aliases coenocliner-package
-##' @keywords package
-##' @seealso \code{\link{coenocline}} for simulating species data, \code{\link{distributions}} for details of the error distributions tha can be used for simulations, and \code{\link{species-response}} for details on the available species response models and the parameters required to use them.
+##' @name download
+##' @keywords methods
+NULL
+
