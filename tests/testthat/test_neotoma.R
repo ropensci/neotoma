09ef7d5d80051df91dfd2e666027b60baab1992c
Gavin Simpson <ucfagls@gmail.com>
2015-02-27 10:00:41 -0600

    yet more narrowing down, again

--- tests/testthat/test_neotoma.R
+++ tests/testthat/test_neotoma.R
@@ -27,18 +27,18 @@ test_that('get_contact accepts and returns the right data types',
 
 #-----------------------------------------------------
 
-context('get_downloads works as expected')
+## context('get_downloads works as expected')
 
-test_that('get_download accepts numeric values and returns values as expected',
-{
-  expect_error(get_download('a'))
-  expect_error(get_download(factor('a')))
-  expect_error(get_download(c('a', 'b')))
-  expect_message(get_download(1), 'API call was successful')
-  expect_that(length(get_download(1)) == 1, is_true())
-  expect_that(length(get_download(c(1,2))) == 2, is_true())
-  expect_is(get_download(1, verbose=FALSE), 'download_list')
-})
+## test_that('get_download accepts numeric values and returns values as expected',
+## {
+##   expect_error(get_download('a'))
+##   expect_error(get_download(factor('a')))
+##   expect_error(get_download(c('a', 'b')))
+##   expect_message(get_download(1), 'API call was successful')
+##   expect_that(length(get_download(1)) == 1, is_true())
+##   expect_that(length(get_download(c(1,2))) == 2, is_true())
+##   expect_is(get_download(1, verbose=FALSE), 'download_list')
+## })
 
 #-----------------------------------------------------
 
