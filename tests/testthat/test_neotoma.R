df0f28e0ccd7fd848569e1fadc930d5f21cd6ab1
Gavin Simpson <ucfagls@gmail.com>
2015-02-26 22:14:43 -0600

    reinstate first test in test_neotoma

--- tests/testthat/test_neotoma.R
+++ tests/testthat/test_neotoma.R
@@ -1,16 +1,16 @@
 #  Tests for the neotoma package.  Mostly validating that changes to the functions
 #  do not break the requirements for data formatting.
 
-## library("testthat")
-## library("neotoma")
+library("testthat")
+library("neotoma")
 
-## context('get_site works as expected')
+context('get_site works as expected')
 
-## test_that('get_site accepts and returns the right data types',
-## {
-##   expect_that('site' %in% class(get_site()), is_true())
-##   expect_that('site' %in% class(get_site(get_download(1))), shows_message('API call was'))
-## })
+test_that('get_site accepts and returns the right data types',
+{
+  expect_that('site' %in% class(get_site()), is_true())
+  expect_that('site' %in% class(get_site(get_download(1))), shows_message('API call was'))
+})
 
 ## context('get_contact work as expected')
 
