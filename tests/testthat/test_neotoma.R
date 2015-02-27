c0181b70716d2d3e6d5228ac808cb2d0bd9ff2b8
Gavin Simpson <ucfagls@gmail.com>
2015-02-27 08:53:11 -0600

    yet more narrowing down

--- tests/testthat/test_neotoma.R
+++ tests/testthat/test_neotoma.R
@@ -83,12 +83,12 @@ test_that('get_download accepts numeric values and returns values as expected',
 
 #-----------------------------------------------------
 
-context('Compiling objects and returning what is expected:')
-test_that('Compiling',
-{
-  expect_is(compile_downloads(get_download(100:103)), 'data.frame')
-  expect_is(compile_downloads(get_download(4559:4564)), 'data.frame')
-  expect_is(compile_taxa(get_download(100), 'P25'), 'download_list')
-  expect_is(compile_taxa(get_download(100)[[1]], 'P25'), 'download')
-})
+## context('Compiling objects and returning what is expected:')
+## test_that('Compiling',
+## {
+##   expect_is(compile_downloads(get_download(100:103)), 'data.frame')
+##   expect_is(compile_downloads(get_download(4559:4564)), 'data.frame')
+##   expect_is(compile_taxa(get_download(100), 'P25'), 'download_list')
+##   expect_is(compile_taxa(get_download(100)[[1]], 'P25'), 'download')
+## })
 
