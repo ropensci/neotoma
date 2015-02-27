0520a54529a2558b60d9652ddaa750bae7b97df4
Gavin Simpson <ucfagls@gmail.com>
2015-02-27 13:33:48 -0600

    yet more narrowing down, again, and again, but getting closer still

--- tests/testthat/test_neotoma.R
+++ tests/testthat/test_neotoma.R
@@ -13,18 +13,18 @@ test_that('get_site accepts and returns the right data types',
                       shows_message('API call was'))
       })
 
-context('get_contact work as expected')
+## context('get_contact work as expected')
 
-test_that('get_contact accepts and returns the right data types',
-{
-  expect_error(get_contact(contactid='aaa'))
-  expect_error(get_contact(contactname=12))
-  expect_error(get_contact(contactstatus=1))
-  expect_error(get_contact(familyname=12))
-  expect_message(get_contact(contactid=1), 'The API call')
-  expect_message(get_contact(familyname='Smith'), 'The API call')
-  expect_message(get_contact(contactname='*Smith*'), 'The API call')
-})
+## test_that('get_contact accepts and returns the right data types',
+## {
+##   expect_error(get_contact(contactid='aaa'))
+##   expect_error(get_contact(contactname=12))
+##   expect_error(get_contact(contactstatus=1))
+##   expect_error(get_contact(familyname=12))
+##   expect_message(get_contact(contactid=1), 'The API call')
+##   expect_message(get_contact(familyname='Smith'), 'The API call')
+##   expect_message(get_contact(contactname='*Smith*'), 'The API call')
+## })
 
 #-----------------------------------------------------
 
