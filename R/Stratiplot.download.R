04e1f8fc86f230abaf6078e2872ca10503d170be
Simon <simon.j.goring@gmail.com>
2016-07-31 21:26:35 -0700

    Added a `Stratiplot` method (from @gavinsimpson's `analogue` R package) for `download` objects.  I can add a `download_list` method as well.
    Just starting to build out the package a bit more, especially as I'm developing the [Workshop](https://github.com/NeotomaDB/Workshops) templates.

---
+++ R/Stratiplot.download.R
@@ -0,0 +1,41 @@
+#' @title Palaeoecological stratigraphic diagrams
+#' @description Draws paleoecological diagrams from a \code{download} object.  Allows control of variable type (using the \code{tran} function from the \code{analogue} package), and taxonomic grouping.
+#' @param x A \code{download} object.
+#' @param yaxis One of the columns in \code{sample.meta}, including \code{depth}, \code{age}, \code{age.younger}, or \code{age.older}, default \code{age}.
+#' @param type An option for axis transformation using \code{tran} from the \code{analogue} package.
+#' @param group An ecological group from the taxon table.  
+#' @param ... variables to be passed to \code{Stratiplot}.
+#' @details A wrapper for the \code{analogue} package's \code{Stratiplot} function.  Allowing the user to plot a stratigraphic diagram directly from a \code{download} object.
+#' @examples
+#' \dontrun{
+#' lake_o_dl <- get_download(15925)
+#' Stratiplot(lake_o_dl)
+#' }
+#' @importFrom analogue Stratiplot tran
+#'@export
+#'
+Stratiplot.download <- function(x, yaxis = "age", type = NA, group = NULL, ...) {
+  counts <- x$counts
+  
+  if (!yaxis %in% c("depth", "age.older", "age.younger", "age")) {
+    stop("You must provide a suitable variable name for the y axis.")
+  }
+  
+  if (!"sample.meta" %in% names(x) | all(is.na(x$sample.meta[,yaxis]))) {
+    stop("This download has no suitable chronological or depth information.")
+  }
+  
+  if (!is.null(group)) {
+    taxa <- x$taxon.list$taxon.name[x$taxon.list$ecological.group %in% group]
+  } else {
+    taxa <- x$taxon.list$taxon.name[!x$taxon.list$ecological.group %in% "LABO" ]
+    
+  }
+  
+  y <- x$sample.meta[,yaxis]
+  
+  counts <- analogue::tran(counts[,taxa], method = type)
+  
+  analogue::Stratiplot(counts, y, ...)
+  
+}
